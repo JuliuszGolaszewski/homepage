57dcc9ed4609b89f2e97a326f6d4fc950a5b12d2
JuliuszGolaszewski <juliusz.golaszewski@gmail.com>
2023-07-27 12:25:23 +0200

    week 7 homework - Node, NPM, SASS

--- main.js
+++
@@ -1,9 +0,0 @@
-const human = {
-    Introduction: (name, age) =>{
-        console.log(`Hello, my name is ${name} and I am ${age} years old.`);
-    }
-}
-
-human.Introduction('Adam', 16);
-human.Introduction('Wiktor', 16);
-human.Introduction('Grzesiek', 16);
